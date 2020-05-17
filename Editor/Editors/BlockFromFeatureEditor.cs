87cabced0738624a856f28c124c9f657ef0b32dc
Elmar Jansen <elmar@elmarjansen.nl>
2020-05-17 18:39:08 +0200

    Several improvements to Editor UI to facilitate block creation debugging

--- Editor/Editors/BlockFromFeatureEditor.cs
+++ Editor/Editors/BlockFromFeatureEditor.cs
@@ -1,7 +1,7 @@
 //C# Example (LookAtPointEditor.cs)
 using UnityEngine;
 using UnityEditor;
-
+using System.Collections.Generic;
 
 namespace GeoJsonCityBuilder.Editor
 {
@@ -15,6 +15,9 @@ namespace GeoJsonCityBuilder.Editor
         SerializedProperty sideMaterial;
         SerializedProperty height;
         SerializedProperty sideUvUnwrapSettings;
+        SerializedProperty pointedRoof;
+        SerializedProperty pointedRoofHeight;
+
 
         void OnEnable()
         {
@@ -23,10 +26,14 @@ namespace GeoJsonCityBuilder.Editor
             sideMaterial = serializedObject.FindProperty("sideMaterial");
             sideUvUnwrapSettings = serializedObject.FindProperty("sideUvUnwrapSettings");
             height = serializedObject.FindProperty("height");
+            pointedRoof = serializedObject.FindProperty("pointedRoof");
+            pointedRoofHeight = serializedObject.FindProperty("pointedRoofHeight");
         }
 
         public override void OnInspectorGUI()
         {
+            var controller = this.serializedObject.targetObject as BlockFromFeature;
+
             serializedObject.Update();
             EditorGUILayout.PropertyField(height);
             EditorGUILayout.Separator();
@@ -34,21 +41,43 @@ namespace GeoJsonCityBuilder.Editor
             EditorGUILayout.PropertyField(bottomMaterial);
             EditorGUILayout.PropertyField(sideMaterial);
             EditorGUILayout.PropertyField(sideUvUnwrapSettings);
+            EditorGUILayout.PropertyField(pointedRoof);
+            EditorGUILayout.PropertyField(pointedRoofHeight);
+            if (controller.floor != null) {
+                EditorGUILayout.BeginFoldoutHeaderGroup(false, "Polygon");
+                foreach(var coordinate in (this.serializedObject.targetObject as BlockFromFeature).floor)
+                {
+                    EditorGUILayout.LabelField(coordinate.ToString());
+                }
+                EditorGUILayout.EndFoldoutHeaderGroup();
+            }
+            // EditorGUILayout.PropertyField(floor);
             EditorGUILayout.BeginHorizontal();
+            if (GUILayout.Button("Set Test Floor"))
+            {
+                this.CreateTestFloor();
+            }
             if (GUILayout.Button("Draw"))
             {
-                var controller = this.serializedObject.targetObject as BlockFromFeature;
-
                 controller.Draw();
             }
             if (GUILayout.Button("Add Rooftop"))
             {
-                var controller = this.serializedObject.targetObject as BlockFromFeature;
-
                 controller.AddPointedRoof();
             }
             EditorGUILayout.EndHorizontal();
             serializedObject.ApplyModifiedProperties();
+        }
+
+        private void CreateTestFloor() {
+            var controller = this.target as BlockFromFeature;
+
+            controller.floor = new List<Vector3>() {
+                new Vector3( 5f, 0f, 10f),
+                new Vector3(-5f, 0f, 10f),
+                new Vector3(-5f, 0f,-10f),
+                new Vector3( 5f, 0f,-10f)
+            };
         }
     }
 }
